74b8fc954e6c8adf4d4d80942be166aed26631a7
fmings <mingsfelicia@gmail.com>
2024-03-08 18:39:44 -0600

    added and styled welcome message

--- components/buttons/loginButton.js
+++ components/buttons/loginButton.js
@@ -2,7 +2,7 @@ import { signIn } from '../../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = '<div id="welcome-message">Welcome to Vocab-YOU-lary!</div><button id="google-auth" class="btn btn-danger google-login">GOOGLE LOGIN</button>';
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
