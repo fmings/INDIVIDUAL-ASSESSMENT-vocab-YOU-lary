fb20879621b1131f4c94f6bc8107289f62601198
fmings <mingsfelicia@gmail.com>
2024-03-09 09:24:26 -0600

    added search feature

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,6 +1,8 @@
+import searchVocabCards from '../api/mergedData';
 import { getVocabCards } from '../api/vocabData';
 import addVocabCardForm from '../components/forms/addVocabCardForm';
 import showVocabCards from '../pages/vocab';
+import renderToDom from '../utils/renderToDom';
 
 const navigationEvents = (uid) => {
   document.querySelector('#view-all').addEventListener('click', () => {
@@ -9,6 +11,23 @@ const navigationEvents = (uid) => {
 
   document.querySelector('#add-card').addEventListener('click', () => {
     addVocabCardForm(uid);
+  });
+
+  document.querySelector('#search').addEventListener('keyup', (e) => {
+    const searchValue = document.querySelector('#search').value.toLowerCase();
+
+    if (e.keyCode === 13) {
+      searchVocabCards(uid, searchValue).then(({ vocab, language }) => {
+        if (vocab.length > 0) {
+          showVocabCards(vocab, uid);
+        } else if (language.lenth > 0) {
+          showVocabCards(language, uid);
+        } else {
+          const domString = '<h1>No Results</h1>';
+          renderToDom('#card-container', domString);
+        }
+      });
+    }
   });
 };
 
